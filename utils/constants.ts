013b9b9bc597d4cf33fc92f43109f6c8ef987375
jansugint <jan@exopek.de>
2025-09-01 07:07:09 +0200

    feat: fix

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,8 @@
 // 'https://exopekdemo-araxgmg9gqf6eyds.germanywestcentral-01.azurewebsites.net/api'
 
 export const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'htts://api.exopek.de/api' 
-  : 'htts://api.exopek.de/api'
+  ? 'https://api.exopek.de/api' 
+  : 'https://api.exopek.de/api'
 
 export const API_ENDPOINTS = {
   AUTH: {
